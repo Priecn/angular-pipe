bac29591005f891ff9dadd9a8fd14b1c7dd9d045
Priecn <prince.silverarrow@gmail.com>
2018-06-20 00:37:06 +0530

    Initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Server } from './server.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  servers: Server[] = [
+    new Server('Production Server', 'medium', 'stable', new Date(15, 1, 2017)),
+    new Server('User Server', 'large', 'stable', new Date(15, 1, 2017)),
+    new Server('Development Server', 'small', 'offline', new Date(15, 1, 2017)),
+    new Server('Testing Environment Server', 'small', 'stable', new Date(15, 1, 2017))
+  ];
+
+  getStatusClasses(server: Server){
+    return {
+      'list-group-item-success': server.status === 'stable',
+      'list-group-item-warning': server.status === 'offline',
+      'list-group-item-danger': server.status === 'critical'
+    };
+  }
 }
---
+++ src/app/server.model.ts
@@ -0,0 +1,8 @@
+export class Server {
+    constructor(public name: string,
+                public instanceType: string,
+                public status: string,
+                public started: Date){
+
+    }
+}
